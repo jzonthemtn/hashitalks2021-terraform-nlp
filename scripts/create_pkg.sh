7b111e385c0808416980fda92996d7c33a748cfd
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-01-30 11:46:02 -0500

    Working on lambda packaging.

--- scripts/create_pkg.sh
+++ scripts/create_pkg.sh
@@ -1,39 +1,12 @@
 #!/bin/bash
+set -e
 
-echo "Executing create_pkg.sh..."
+rm -rf ./lambda_dist_pkg
+mkdir lambda_dist_pkg
 
-dir_name=lambda_dist_pkg/
-mkdir -p $dir_name
-
-# Create and activate virtual environment...
-python3 -m venv ./$dir_name/venv
-source $dir_name/venv/bin/activate
-
-# Installing python dependencies...
-FILE=$dir_name/lambda_function/requirements.txt
-
-if [ -f "$FILE" ]; then
-  echo "Installing dependencies..."
-  echo "From: requirement.txt file exists..."
-  pip install -r "$FILE"
-
-else
-  echo "Error: requirement.txt does not exist!"
-fi
-
-# Deactivate virtual environment...
+python3 -m venv ./lambda_dist_pkg/venv
+source lambda_dist_pkg/venv/bin/activate
+python3 -m pip install -r ./lambda_function/requirements.txt
 deactivate
 
-# Create deployment package...
-echo "Creating deployment package..."
-cp $dir_name/venv/lib/$runtime/site-packages/
-
-#cd env_$function_name/lib/$runtime/site-packages/
-#cp -r . $path_cwd/$dir_name
-#cp -r $path_cwd/lambda_function/ $path_cwd/$dir_name
-
-# Removing virtual environment folder...
-echo "Removing virtual environment folder..."
-rm -rf ./$dir_name/venv
-
-echo "Finished script execution!"
+cp -r lambda_dist_pkg/venv/lib/python3.8/site-packages/ ./lambda_dist_pkg/
