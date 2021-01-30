37af6f4037f35dd35cdb2ade35c75f663ce5b9af
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-01-30 16:43:38 -0500

    Updating sqs send message.

--- queue-training.sh
+++ queue-training.sh
@@ -3,3 +3,6 @@ set -e
 QUEUE_URL=`terraform output -raw queue_url`
 
 echo "Publishing message to SQS queue $QUEUE_URL"
+aws sqs send-message \
+  --queue-url $QUEUE_URL \
+  --message-body '{"name": "my-model"}'
