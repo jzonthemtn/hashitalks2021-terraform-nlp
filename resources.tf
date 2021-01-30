f3b0c42bc98aa3c279ed0e7889e88607cd6fd631
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-01-30 12:45:28 -0500

    Setting queue visibility to match lambda function.

--- resources.tf
+++ resources.tf
@@ -1,11 +1,12 @@
 resource "aws_sqs_queue" "ml_queue" {
-  name                      = "ml-queue"
-  delay_seconds             = 10
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  name                       = "ml-queue"
+  delay_seconds              = 10
+  max_message_size           = 2048
+  message_retention_seconds  = 86400
+  receive_wait_time_seconds  = 10
+  visibility_timeout_seconds = 60
 }
 
 resource "aws_s3_bucket" "ml-bucket" {
-  acl    = "private"
+  acl = "private"
 }
