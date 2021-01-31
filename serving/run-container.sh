75e06edb7f0f08b34348ee55454561c204db6912
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-01-31 09:31:31 -0500

    Parameterizing the bucket name.

--- serving/run-container.sh
+++ serving/run-container.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 VERSION=${1:-latest}
+S3_BUCKET=${2:-my-bucket}
 
 docker run \
   --env "AWS_ACCESS_KEY_ID=***" \
   --env "AWS_SECRET_ACCESS_KEY=***" \
   --env "AWS_DEFAULT_REGION=us-east-1" \
-  --env "MODEL_BUCKET=mtnfog-temp" \
+  --env "MODEL_BUCKET=$S3_BUCKET" \
   -p 8080:8080 \
   -it \
   --rm \
