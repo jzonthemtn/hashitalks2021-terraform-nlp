c39d8d700f5397cab8486735dcb4d6924b247a71
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-01-31 08:06:30 -0500

    Working on lambda function.

--- resources.tf
+++ resources.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "ml_queue" {
   name                       = "ml-queue"
   delay_seconds              = 10
   max_message_size           = 2048
-  message_retention_seconds  = 86400
+  message_retention_seconds  = 1209600
   receive_wait_time_seconds  = 10
   visibility_timeout_seconds = 60
 }
