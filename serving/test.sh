330f412562eeddbda245fafe2571fbfb8c261eed
jzonthemtn <jeff.zemerick@mtnfog.com>
2021-02-13 21:16:16 -0500

    Updating test.

--- serving/test.sh
+++ serving/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-HOSTNAME=${1:-localhost}
-curl -vvvv -X POST http://$HOSTNAME:8080/ner --data "George Washington was president of the United States." -H "Content-type: text/plain"
+HOSTNAME=${1:-localhost:8080}
+curl -vvvv -X POST http://$HOSTNAME/ner --data "George Washington was president of the United States." -H "Content-type: text/plain"
